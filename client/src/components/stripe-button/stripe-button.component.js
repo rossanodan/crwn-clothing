1cf9dcb6c766f5a133801151ef17ea9e66bd34b3
rossanodan <rossanodan@gmail.com>
2020-06-22 21:46:24 +0100

    Separating client and server and added express server to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Axios from "axios";
 import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_Mv18VocJjKAMUBqAPLQ88OLi00xRSau8Bj";
   const onToken = (token) => {
-    alert("Payment successful!");
+    Axios.post("/payment", {
+      token,
+      amount: priceForStripe,
+    })
+      .then((reponse) => {
+        console.log(reponse);
+        alert("Payment successful!");
+      })
+      .catch((error) => {
+        console.log("Payment error:", JSON.parse(error));
+        alert("There was an issue with your payment.");
+      });
   };
 
   return (
